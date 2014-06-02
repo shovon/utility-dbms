26a47f219099285bef452380f4630af85f53764d
Salehen Shovon Rahman <salehen.rahman@gmail.com>
2014-06-02 13:50:06 -0700

    I got data going in.

--- app.js
+++ app.js
@@ -26,6 +26,52 @@ multistatementConnection.connect();
 
 var app = express();
 
+// The device ID in the database, and the device ID from the house can be
+// entirely different. Hence there should be a physical device ID to database
+// device ID mapping.
+// TODO: variable caching is really bad. The devices should also be cached in
+//   redis.
+var devicesMapping = [];
+function getDevice(id, series, callback) {
+  var device = devicesMapping[[id, series].join(':')];
+
+  // This means that the device was never cached, and hence we have to query the
+  // database.
+  if (!device) {
+    return singlestatementConnection.query(
+      'SELECT * FROM devices WHERE real_device_id = ? AND type = ?;',
+      [id, series],
+      function (err, result) {
+        if (err) { return callback(err); }
+
+        // This means that the device is not even in the database. Insert it,
+        // and get the row from the database.
+        if (!result || !result.length) {
+          // TODO: instead of returning null, just insert the device into the
+          //   database.
+          return singlestatementConnection.query(
+            // TODO: check to see whether the insertion and retrieval can be
+            //   done in a single statement.
+            'INSERT INTO devices (real_device_id, type) VALUES (?, ?)',
+            [id, series],
+            function (err) {
+              if (err) { return callback(err); }
+              getDevice(id, series, callback);
+            }
+          );
+        }
+        var device = result[0];
+        devicesMapping[[id, series].join(':')] = device;
+        callback(null, device);
+      }
+    );
+  }
+
+  setImmediate(function () {
+    callback(null, device);
+  });
+}
+
 app.use(bodyParser.json());
 
 app.get('/data/:series', function (req, res, next) {
@@ -50,171 +96,81 @@ app.post('/data', function (req, res, next) {
 
   async.waterfall([
 
-    // Store the data point devices into the database.
-    function (callback) {
-      // TODO: cache the devices query so that we don't end up having to query
-      //   the database direclty every time when we want to see wether or not
-      //   any given device exists in the database.
-
-      // Extract the consumption value's device ID only.
-      var devices = req.body.map(function (point) {
-        // Do note that `point.device_id` is the ID that is identified in the
-        // physical device itself. The device ID that will be stored in the
-        // database is an index to a particular row.
-        return [ point.device_id, point.series ];
-      });
-
-      // The WHERE statement.
-      //
-      // It should generate a string that looks like
-      //
-      //     (real_device_id = ? AND type = ?) OR
-      //       (real_device_id = ? AND type = ?) OR ... OR 
-      //       (real_device_id = ? AND type = ?)
-      //
-      // for N devices.
-      var where = devices
-        .map(function (device) {
-          return mysql.format('(real_device_id = ? AND type = ?)', device)
-        })
-        .join(' OR ');
-
-      // The query.
-      var query = util
-        .format(
-          'SELECT * FROM devices WHERE %s',
-          where
-        );
-
-      // Query the devices.
-      singlestatementConnection.query(
-        query,
-        function (err, result) {
-          if (err) { return callback(err); }
-
-          // Instead of calling `callback`, call this function. Keeps the
-          // `callback` call DRY.
-          function finishCallback() {
-            callback(null, result);
-          }
-
-          // Extract the device's consumption value's device ID only.
-          var databaseDevices = result.map(function (device) {
-            return JSON.stringify({
-              real_device_id: device.real_device_id,
-              type: device.type
-            });
-          });
-
-          var devicesTupleString = devices.map(function (device) {
-            return JSON.stringify({
-              real_device_id: device[0],
-              type: device[1]
-            });
-          });
-
-          // Get all device IDs that are not in the database.
-          var intersection = _.intersection(databaseDevices, devicesTupleString);
-          var toInsert = _.difference(devicesTupleString, intersection);
-
-          // Nothing to insert. Move on.
-          if (!toInsert.length) {
-            return setImmediate(finishCallback);
-          }
-
-          // Generate an insertion query for all devices that are not in the
-          // database yet.
-          // TODO: the query can be completed in a single statement.
-          var insertionQuery = toInsert.map(function(device) {
-            device = JSON.parse(device);
-            var arr = [
-              device.real_device_id,
-              device.type
-            ]
-            return mysql.format(
-              'INSERT INTO devices (real_device_id, type) VALUES (?, ?)', arr
-            );
-          }).join(';');
-
-          // Now perform the insertion.
-          multistatementConnection.query(insertionQuery, function (err, result) {
-            if (err) { return callback(err); }
-            finishCallback();
-          });
-        }
-      )
-    },
-
     // Store the actual data into the database.
     //
     // Remember, the `devices` parameter reperesents rows from the `devices`
     // table, with all columns loaded.
-    function (devices, callback) {
-      // This is where we will be storing the devices retrieved from the
-      // database. They will be indexed by the string
-      //
-      //     <real device ID>:<series name>
-      //
-      // This exists so that we can get a particular device row's ID column
-      // value more quickly.
-      var devicesHash = {};
-      devices.forEach(function (device) {
-        devicesHash[device.real_device_id + ':' + device.type] = device;
-      });
+    function (callback) {
 
       async.each(req.body, function (item, callback) {
-        databaseDeviceId = devicesHash[item.device_id + ':' + item.series].id
 
-        // Start out by getting the most recently inserted data point.
-        // TODO: so far, we are issuing a query for every device. It shouldn't
-        //   many queryes, but just one query for all the devices that are
-        //   having their values inserted. Of course, for the entire list of
-        //   devices, there will undoubtedly be more than one queries
-        //   (hopefully at most two), but it's **not** going to be per device.
-        singlestatementConnection.query(
-          'SELECT * FROM data_points WHERE device_id = ? \
-          ORDER BY time DESC LIMIT 1',
-          [databaseDeviceId],
-          function (result, err) {
-            // TODO: this shouldn't crash. Just gracefully move on.
-            if (err) { return callback(err); }
+        async.waterfall([
+          // This gets the device, and its corresponding ID in the database
+          // table.
+          function (callback) {
+            getDevice(item.device_id, item.series, callback)
+          },
 
-            // Extract the row, if one exists. If it doesn't exist, then the row
-            // is nil.
-            var row = result && result.lenth ? result[0] : null;
+          // Get the most recently inserted data point.
+          function (device, callback) {
+            singlestatementConnection.query(
+              'SELECT * FROM data_points WHERE device_id = ? \
+              ORDER BY time DESC LIMIT 1',
+              [device.id],
+              function (err, result) {
+                if (err) { return callback(err); }
+                var row = result && result.length ? result[0] : null
+                callback(null, row);
+              }
+            )
+          },
+
+          // Finally, insert the data point that the client provided, as well as
+          // update the running total.
+          function (row, callback) {
+            var insertionQuery =
+              mysql.format(
+                'INSERT INTO data_points ( \
+                  device_id, \
+                  value, \
+                  running_total, \
+                  time \
+                ) VALUES (?, ?, ?, FROM_UNIXTIME(?))',
+                [
+                  row.device_id,
+                  item.value,
+                  // Add the to-be inserted value to the running total, if there
+                  // was a row found. Otherwise, just add to 0.
+                  row ? row.running_total + item.value : item.value,
+                  // Although, we won't be inserting timestamps, however, it's
+                  // much easier to have MySQL convert the Unix timestamp, right
+                  // before inserting the data. Maybe I will do a more
+                  // appropriate insertion in the future.
+                  new Date(item.time).getTime() / 1000
+                ]
+              );
 
             // Now do the actual insertion.
-            singleStatementConnection.query(
-              'INSERT INTO data_points (device_id, value, running_total, time) \
-              VALUES (?, ?, ?, FROM_UNIXTIME(?))'
-              [
-                databaseDeviceId,
-                item.value,
-                // Add the to-be inserted value to the running total, if there
-                // was a row found. Otherwise, just add to 0.
-                row ? row.running_total + item.value : item.value,
-                // Although, we won't be inserting timestamps, however, it's
-                // much easier to have MySQL convert the Unix timestamp, right
-                // before inserting the data. Maybe I will do a more appropriate
-                // insertion in the future.
-                new Date(item.time).getTime() / 1000
-              ],
-              function (result, err) {
+            singlestatementConnection.query(insertionQuery,
+              function (err, result) {
                 // TODO: this shouldn't crash. Just gracefully move on.
                 if (err) { return callback(err); }
                 callback(null);
               }
-            )
+            );
           }
-        );
+        ], function (err) {
+          if (err) { return callback(err); }
+          callback(null);
+        })
       }, function (err) {
         if (err) { return callback(err); }
-        callback(devices);
+        callback(null);
       });
     },
 
     // Roll up the readings into buckets.
-    function (devices, callback) {
+    function (callback) {
       callback(new Error('Not yet implemented.'));
     }
 
